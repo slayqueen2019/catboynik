9a28ac816206bb468b0e7790d267cf0270fd9961
slayqueen2019 <159428733+slayqueen2019@users.noreply.github.com>
2024-02-11 01:27:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function(){
+output.innerHTML = 'Хехех я подустала, хочу спать. Надеюсь тебе понравилось. Короче.. тот чел,у которого я проходила этот мини-курс, делает еще большой курс, но он стоит 5к. Объясняет он прям хорошо, для чайников (меня) очень понятно, мне прям нравится. Я вот даже java script использовала. Как думаешь, стоит или не стоит? Жду ответ в тг))';
+});
